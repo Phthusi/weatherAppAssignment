c212ee48c3a8350fa25926bd86eea4cfd7a867ed
phumulani thusi <thusiphumulani1@gmail.com>
2024-12-24 17:26:48 +0200

    changed the iu and took care of the edge case

--- mysite/weatherapp/views.py
+++ mysite/weatherapp/views.py
@@ -1,31 +1,39 @@
 import urllib.request
 import json
 from django.shortcuts import render
-# Create your views here.
-
 
 def index(request):
+    data = {}
+    error_message = None
 
     if request.method == 'POST':
         city = request.POST['city']
 
-        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=<API-KEY-GOES-HERE>').read()
-        list_of_data = json.loads(source)
+        try:
+            source = urllib.request.urlopen(
+                'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=e7eabdf45f0299cd46884cf249371561'
+            ).read()
+            list_of_data = json.loads(source)
 
-        data = {
-            "country_code": str(list_of_data['sys']['country']),
-            "coordinate": str(list_of_data['coord']['lon']) + ', '
-            + str(list_of_data['coord']['lat']),
+            # Check if the response contains a 404 error
+            if list_of_data.get('cod') != 200:
+                error_message = list_of_data.get('message', 'City not found.')
+            else:
+                data = {
+                    "country_code": str(list_of_data['sys']['country']),
+                    "coordinate": str(list_of_data['coord']['lon']) + ', ' + str(list_of_data['coord']['lat']),
+                    "temp": str(list_of_data['main']['temp']) + ' °C',
+                    "pressure": str(list_of_data['main']['pressure']),
+                    "humidity": str(list_of_data['main']['humidity']),
+                    'main': str(list_of_data['weather'][0]['main']),
+                    'description': str(list_of_data['weather'][0]['description']),
+                    'icon': list_of_data['weather'][0]['icon'],
+                }
+        except Exception as e:
+            error_message = "An error occurred while processing your request."
 
-            "temp": str(list_of_data['main']['temp']) + ' °C',
-            "pressure": str(list_of_data['main']['pressure']),
-            "humidity": str(list_of_data['main']['humidity']),
-            'main': str(list_of_data['weather'][0]['main']),
-            'description': str(list_of_data['weather'][0]['description']),
-            'icon': list_of_data['weather'][0]['icon'],
-        }
-        print(data)
-    else:
-        data = {}
-
-    return render(request, "main/index.html", data)
+    context = {
+        'data': data,
+        'error_message': error_message
+    }
+    return render(request, "main/index.html", context)
